8642b6929c91b74677c60236e95efd2d8f491349
lixiangqian02 <lixiangqian02@meituan.com>
2015-11-01 20:37:41 +0800

    commit unsaved

--- Chapter 1/1.2/n1_2_1.scala
+++ Chapter 1/1.2/n1_2_1.scala
@@ -1,3 +1,10 @@
+/*
+* 递归计算过程
+* 线性递归过程
+迭代计算过程就是那种其状态可以在从一个状态到下一个状态转换时，这些变量的更新方式；还有一个（可能有的）结束监测。
+所有的迭代过程都可以『以硬件的方式』实现为一个机器，其中只有固定数目的寄存器，无须任何辅助寄存器。
+Scheme总能在常量空间中执行迭代计算过程，即使这一计算是用一个递归过程描述的。具有这一特性的实现称为尾递归。
+*/
 object N1_2_1 {
     def fibonacciRecursive(n: Long): Long = {
          n match {
@@ -22,7 +29,7 @@ object N1_2_1 {
         println(fibonacciRecursive(13))
         println("Recurise mode takes " + (System.currentTimeMillis - start) + " ms to compute 23!")
         start = System.currentTimeMillis
-        println(fibonacciLinearRecursive(13))
+        println(fibonacciLinearRecursive(13))b
         println("Linear recurise mode takes " + (System.currentTimeMillis - start) + " ms to compute 23!")
     }
 }
